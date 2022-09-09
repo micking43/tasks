0c0f0d38520c1af1e86160875e138f569a1675a0
Michael King <micking@udel.edu>
2022-09-08 22:05:14 -0400

    Again

--- src/App.tsx
+++ src/App.tsx
@@ -13,41 +13,51 @@ function App(): JSX.Element {
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
-            <Container>
-                <Row>
-                    <Col>
-                        <img
-                            src="C:\Users\micking\tasks\public\IMG_1715.jpg"
-                            alt="A picture of abandoned food my friend took"
-                        />
-                        <div
-                            style={{
-                                backgroundColor: "red",
-                                width: "1",
-                                height: "1"
-                            }}
-                        ></div>
-                    </Col>
-                    <Col>
-                        <Button onClick={() => console.log("Hello World!")}>
-                            Log Hello World
-                        </Button>
-                        Ordered List:
-                        <ol>
-                            <li>First</li>
-                            <li>Next</li>
-                            <li>Last</li>
-                        </ol>
-                        <div
-                            style={{
-                                backgroundColor: "red",
-                                width: "1",
-                                height: "1"
-                            }}
-                        ></div>
-                    </Col>
-                </Row>
-            </Container>
+            <div>
+                <Container>
+                    <Row>
+                        <Col>
+                            <div>
+                                <img
+                                    src="C:\Users\micking\tasks\public\IMG_1715.jpg"
+                                    alt="A picture of abandoned food my friend took"
+                                />
+                            </div>
+                            <div
+                                style={{
+                                    backgroundColor: "red",
+                                    width: "1",
+                                    height: "1"
+                                }}
+                            ></div>
+                        </Col>
+                        <Col>
+                            <div>
+                                <Button
+                                    onClick={() => console.log("Hello World!")}
+                                >
+                                    Log Hello World
+                                </Button>
+                            </div>
+                            <div>
+                                Ordered List:
+                                <ol>
+                                    <li>First</li>
+                                    <li>Next</li>
+                                    <li>Last</li>
+                                </ol>
+                            </div>
+                            <div
+                                style={{
+                                    backgroundColor: "red",
+                                    width: "1",
+                                    height: "1"
+                                }}
+                            ></div>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
         </div>
     );
 }
