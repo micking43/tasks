bb94af72699f6b4734bf34b2d225b31590ff89e6
Michael King <micking@udel.edu>
2022-10-04 16:37:47 -0400

    one left

--- src/components/TwoDice.tsx
+++ src/components/TwoDice.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
+import { disposeEmitNodes } from "typescript";
 
 /**
  * Here is a helper function you *must* use to "roll" your die.
@@ -12,5 +13,31 @@ export function d6(): number {
 }
 
 export function TwoDice(): JSX.Element {
-    return <div>Two Dice</div>;
+    const [leftDie, setLeftDie] = useState<number>(1);
+    const [rightDie, setRightDie] = useState<number>(6);
+    return (
+        <div>
+            <div>
+                left-die: <span>{leftDie}</span>
+            </div>
+            <div>
+                right-die: <span>{rightDie}</span>
+            </div>
+            <div>
+                <Button onClick={() => setLeftDie(d6())}>Roll Left</Button>
+            </div>
+            <div>
+                <Button onClick={() => setRightDie(d6())}>Roll Right</Button>
+            </div>
+            <div>
+                {leftDie === 1 && rightDie === 1 ? (
+                    <span>Lose</span>
+                ) : leftDie === rightDie ? (
+                    <span>Win</span>
+                ) : (
+                    <span> </span>
+                )}
+            </div>
+        </div>
+    );
 }
