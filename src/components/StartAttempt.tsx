a460f78de46167a0b24124e8acf40a2788e5042c
Michael King <micking@udel.edu>
2022-10-04 16:09:18 -0400

    two left

--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -2,5 +2,39 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function StartAttempt(): JSX.Element {
-    return <div>Start Attempt</div>;
+    const [attempts, setAttempts] = useState<number>(4);
+    const [progress, setProgress] = useState<boolean>(false);
+    function addOne(): void {
+        setAttempts(attempts + 1);
+    }
+    function minusOne(): void {
+        setAttempts(attempts - 1);
+        setProgress(true);
+    }
+
+    return (
+        <div>
+            <div>
+                Number of Attempts: <span>{attempts}</span>
+            </div>
+            <div>
+                <Button
+                    onClick={minusOne}
+                    disabled={progress || attempts === 0}
+                >
+                    Start Quiz
+                </Button>
+            </div>
+            <div>
+                <Button onClick={() => setProgress(false)} disabled={!progress}>
+                    Stop Quiz
+                </Button>
+            </div>
+            <div>
+                <Button onClick={addOne} disabled={progress}>
+                    Mulligan
+                </Button>
+            </div>
+        </div>
+    );
 }
